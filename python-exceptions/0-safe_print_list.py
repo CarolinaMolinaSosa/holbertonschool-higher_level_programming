cc9b336519aea27c6e6358305300bbe04549c116
CarolinaMolinaSosa <molinasosacarolina17@gmail.com>
2023-01-29 16:11:09 -0800

    fix becouse im stupid

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    cont = 0
     try:
-        for i in my_list:
-            if cont < x:
-                print(f"{i}", end="")
-                cont += 1
-        print(f"")
-    except Exception:
-        pass
-    return
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+    except IndexError:
+        return i
+    else:
+        return x
+    finally:
+        print("")
