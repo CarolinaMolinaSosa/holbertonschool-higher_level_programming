e89cfa4e7780caafa2f7b57aa0e3e3c4c3b5d34c
CarolinaMolinaSosa <molinasosacarolina17@gmail.com>
2023-02-11 13:29:12 -0800

    Forgot documentation add it

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+"""Write an empty class BaseGeometry."""
+
+
 class BaseGeometry:
     pass
--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Write a class BaseGeometry (based on 6-base_geometry.py)."""
+
+
 class BaseGeometry:
     def area(self):
         raise Exception("area() is not implemented")
