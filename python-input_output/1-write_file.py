e1f21c8cada600d35f26ee5dcea6ba90e5965c66
CarolinaMolinaSosa <molinasosacarolina17@gmail.com>
2023-02-12 12:34:35 -0800

    Upload tasks 0 to 5

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Function that writes a string to a text file (UTF8) and
+returns the number of characters written
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Write a string to a UTF8 text file.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
